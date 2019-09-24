5200644dc9cf9de21b8d70af5af7d015abb0b591
DannyBoyNYC <daniel.deverell@gmail.com>
2019-09-24 11:37:49 -0400

    done

--- app/scripts.js
+++ app/scripts.js
@@ -1,29 +1,7 @@
 // Create the dog listing markup
-var createListing = function(dog) {
-  var content =
-    '<h2></h2>' +
-    '<p><img src=""></p>' +
-    '<p>' +
-    'Age: <br>' +
-    'Size: <br>' +
-    'Gender: <br>' +
-    'Breeds: ' +
-    '</p>' +
-    '<strong>Other Details:</strong>' +
-    '<p>Description</p>';
-  return content;
-};
 
 // Create a list of adoptable dogs
-var getDogs = function() {
-  console.log(apiData);
-  return apiData
-    .map(function(dog) {
-      var html = 'Dog! ';
-      return html;
-    })
-    .join('');
-};
+
 // Load list of adoptable dogs into the DOM
 var dogList = document.querySelector('#dogs');
 dogList.innerHTML = getDogs();
