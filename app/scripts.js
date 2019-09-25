27001790985de96075d2b98ae6b9d9ebda471e84
DannyBoyNYC <daniel.deverell@gmail.com>
2019-09-25 12:05:20 -0400

    done

--- app/scripts.js
+++ app/scripts.js
@@ -15,9 +15,17 @@ var getSummary = function() {
     // return breeds to next loop!!!!!
     return breeds;
   }, {});
-  if (Object.keys(breeds).length > 1) {
+  if (Object.keys(breeds).length < 1) {
     return 'yowza';
   }
+  var summary = '';
+  for (var breed in breeds) {
+    if (breeds.hasOwnProperty(breed)) {
+      summary += `<li>${breed} (${breeds[breed]})</li>`;
+    }
+  }
+  return `<h2>Available Breeds</h2>
+  <ul>${summary}</ul>`;
 };
 
 console.log(getSummary());
